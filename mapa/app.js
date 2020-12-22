a680cfb4dec22002bb537fa205932579d6c8e879
vencax <vencax@centrum.cz>
2020-12-22 22:14:35 +0100

    mapa hotova

--- mapa/app.js
+++ mapa/app.js
@@ -1,56 +1,73 @@
 /* global L */
 
-function addPoint (point, map) {  
-  var marker = L.marker([point.lat, point.lng], {icon: icon}).addTo(map);
-  const content = JSON.stringify(point, null, 2)
-  marker.bindPopup(content)
-}
-
-function pridejMapuProjektu (divid, startLat, startLng, backendURL) {
+function pridejMapuProjektu (divid, startLat, startLng, startZoom, backendURL) {
 
   var map = L.map(divid, {
     center: L.latLng(startLat, startLng),
-    zoom: 14
+    zoom: startZoom
   })
+
+  function _makePopupHTML (p) {
+    return '<div><h1>' + p.nazev + '</h1><p>' + p.popis + '</p>'
+      + '<p>Cena: ' + p.cena + '</p>'
+      + '<p>Stadium: ' + p.stadium + '</p></div>'
+  }
+
+  function addPoint (point) {
+    var poloha = point.poloha.split(',');
+    var marker = L.marker(poloha, {icon: getIcon(point.zanr)}).addTo(map);
+    marker.bindPopup(_makePopupHTML(point));
+  }
   
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map)
-  
-  var point = {
-    lat: startLat,
-    lng: startLng
-  }
-  addPoint(point, map)
 
-  // axios.get(backendURL + '')
-  // .then(function(res) {
-
-  // })
-  // .catch(function(err) {
-  //   alert(err)
-  // })
+  axios.get(backendURL + '')
+  .then(function(res) {
+    res.data.map(addPoint)
+  })
+  .catch(function(err) {
+    alert(err)
+  })
   
 }
 
-const myCustomColour = '#583470'
+var colors = {
+  'ziv_pros': '#00ff00',
+  'doprava': '#483737',
+  'bydleni': '#0000ff',
+  'sprava': '#ffff00',
+  'social': '#c10000',
+  'kultura': '#808000',
+  'volnycas': '#585470'
+}
 
-const markerHtmlStyles = `
-  background-color: ${myCustomColour};
-  width: 3rem;
-  height: 3rem;
-  display: block;
-  left: -1.5rem;
-  top: -1.5rem;
-  position: relative;
-  border-radius: 3rem 3rem 0;
-  transform: rotate(45deg);
-  border: 1px solid #FFFFFF`
+const icons = {}
 
-const icon = L.divIcon({
-  className: "my-custom-pin",
-  iconAnchor: [0, 24],
-  labelAnchor: [-6, 0],
-  popupAnchor: [0, -36],
-  html: `<span style="${markerHtmlStyles}" />`
-})
+function getIcon (zanr) {
+  if (icons[zanr]) { return icons[zanr] }
+
+  const markerHtmlStyles = `
+    background-color: ${colors[zanr] || 'red'};
+    width: 3rem;
+    height: 3rem;
+    display: block;
+    left: -1.5rem;
+    top: -1.5rem;
+    position: relative;
+    border-radius: 3rem 3rem 0;
+    transform: rotate(45deg);
+    border: 1px solid #FFFFFF`
+
+  const icon = L.divIcon({
+    className: "my-custom-pin",
+    iconAnchor: [0, 24],
+    labelAnchor: [-6, 0],
+    popupAnchor: [0, -36],
+    html: `<span style="${markerHtmlStyles}" />`
+  })
+  icons[zanr] = icon
+  return icon
+}
+
